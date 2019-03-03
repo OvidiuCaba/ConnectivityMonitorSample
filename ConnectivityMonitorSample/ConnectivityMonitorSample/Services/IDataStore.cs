2eb2ab5638bfc77602ff5fd2b4ca87e3cd418936
Ovidiu Caba <older_ovello@yahoo.com>
2019-03-03 20:03:46 +0200

    Cleanup: remove unneeded code

--- ConnectivityMonitorSample/ConnectivityMonitorSample/Services/IDataStore.cs
+++ ConnectivityMonitorSample/ConnectivityMonitorSample/Services/IDataStore.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ConnectivityMonitorSample.Services
 {
     public interface IDataStore<T>
     {
-        Task<bool> AddItemAsync(T item);
-        Task<bool> UpdateItemAsync(T item);
-        Task<bool> DeleteItemAsync(string id);
         Task<T> GetItemAsync(string id);
         Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
     }
--- ConnectivityMonitorSample/ConnectivityMonitorSample/Services/MockDataStore.cs
+++ ConnectivityMonitorSample/ConnectivityMonitorSample/Services/MockDataStore.cs
@@ -15,42 +15,14 @@ namespace ConnectivityMonitorSample.Services
             items = new List<Item>();
             var mockItems = new List<Item>
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Online and offline functionality", Description="This functionality support both online and offline modes." },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Online functionality", Description="This functionality only support online mode." },
             };
 
             foreach (var item in mockItems)
             {
                 items.Add(item);
             }
-        }
-
-        public async Task<bool> AddItemAsync(Item item)
-        {
-            items.Add(item);
-
-            return await Task.FromResult(true);
-        }
-
-        public async Task<bool> UpdateItemAsync(Item item)
-        {
-            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
-            items.Remove(oldItem);
-            items.Add(item);
-
-            return await Task.FromResult(true);
-        }
-
-        public async Task<bool> DeleteItemAsync(string id)
-        {
-            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
-            items.Remove(oldItem);
-
-            return await Task.FromResult(true);
         }
 
         public async Task<Item> GetItemAsync(string id)
